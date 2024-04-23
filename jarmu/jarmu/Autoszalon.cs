429d64bbee4945bac9a77fcfa7c1d5b96f95824c
Benceeeeeeee <144451434+Benceeeeeeee@users.noreply.github.com>
2024-04-23 12:55:13 +0200

    Add files via upload

---
+++ jarmu/jarmu/Autoszalon.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jarmu
+{
+    internal class Autoszalon
+    {
+        public List<Jarmu> Jarmuvek {  get; set; }
+        public Autoszalon()
+        {
+            Jarmuvek = new List<Jarmu>();
+        }
+        public void UjJarmuHozzaadasa(Jarmu jarmu)
+        {
+            Jarmuvek.Add(jarmu);
+            Console.WriteLine("jármű hozzá lett adva");
+        }
+        public void JarmuEladasa(string gyarto, string modell)
+        {
+            bool kimenet = false;
+            int index = 0;
+            foreach (var jarmu in Jarmuvek)
+            {
+                if (jarmu.Gyarto == gyarto && jarmu.Modell == modell)
+                {
+                    kimenet = true;
+                    Jarmuvek.RemoveAt(index);
+                }
+                else
+                {
+                    kimenet = false;
+                }
+                index++;
+            }
+        }
+        public void JarmuKeresese(string gyarto, string modell)
+        {
+            foreach (var jarmu in Jarmuvek)
+            {
+                if (jarmu.Gyarto == gyarto && jarmu.Modell == modell)
+                {
+                    Console.WriteLine(jarmu);
+                }
+            }
+        }
+        public void JarmuvekListazasa()
+        {
+            foreach(var jarmu in Jarmuvek)
+            {
+                Console.Write(jarmu);
+            }
+        }
+    }
+}
---
+++ jarmu/jarmu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jarmu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Autoszalon jarmuvek = new Autoszalon();
+            Szemelyauto jarmu = new Szemelyauto("Toyota", "Corolla", 2021, 8000000, 15000, 5, 6.5, "Fehér");
+            jarmuvek.UjJarmuHozzaadasa(jarmu);
+
+            Terepjaro terep = new Terepjaro("Land Rove", "Defender", 2020, 15000000, 20000, "4WD", true, 3500);
+            jarmuvek.UjJarmuHozzaadasa(jarmu);
+
+
+            jarmuvek.JarmuEladasa("Toyota", "Corolla");
+
+            jarmuvek.JarmuKeresese("Land Rover", "Defender");
+
+            jarmuvek.JarmuvekListazasa();
+
+        }
+    }
+}
